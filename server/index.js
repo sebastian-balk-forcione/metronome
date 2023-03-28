9589245a39466f9a334e85cd4c7661c89e0d4710
Sebastian Balk-Forcione <sbalkforcione@gmail.com>
2023-03-28 11:45:52 -0400

    inital set up

---
+++ server/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// import the needed node_modules.
+const express = require("express");
+const morgan = require("morgan");
+const port = 8888;
+
+express()
+  .use(morgan("tiny"))
+  .use(express.json())
+
+  // Any requests for static files will go into the public folder
+  .use(express.static("public"))
+
+  .get("/test", (req, res) => {
+    res.status(200).json({ status: 200, success: true });
+  })
+  // this is our catch all endpoint.
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not what you are looking for.",
+    });
+  })
+
+  // Node spins up our server and sets it to listen on port 8000.
+  .listen(port, () => console.log(`Listening on port ${port}`));
